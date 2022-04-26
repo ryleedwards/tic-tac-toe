b1467112cd9fe0ae71d62f976624d7b3af434d1f
Ryle Edwards <ryle.edwards@gmail.com>
2022-04-26 14:31:32 -0500

    Add functions to gameBoard, gameState, and player

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const gameBoard = (() => {
     return _board;
   };
 
+  const getSquare = (position) => {
+    return _board[position];
+  };
+
+  const updateSquare = (position, entry) => {
+    _board.splice(position, 1, entry);
+  };
+
   return {
     getBoard,
+    getSquare,
+    updateSquare,
   };
 })();
-
-console.log(gameBoard.getBoard());
 
 // gameState Module
 const gameState = (() => {
@@ -22,14 +30,34 @@ const gameState = (() => {
   const incrementTurn = () => {
     turn++;
   };
+  const resetTurns = () => {
+    turn = 0;
+  };
   return {
     getTurn,
     incrementTurn,
+    resetTurns,
   };
 })();
 
-const player = (name) => {
+// player Factory
+
+const player = (name, number) => {
+  if (number == 1) icon = "X";
+  else icon = "O";
   let state = {
     name,
+    number,
+    icon,
+  };
+
+  return {
+    greet: () => console.log(`hello my name is ${name}`),
+    getIcon: () => state.icon,
   };
 };
+
+const player1 = player("player1", 1);
+const player2 = player("player2", 2);
+
+gameBoard.updateSquare(0, player1.getIcon());
