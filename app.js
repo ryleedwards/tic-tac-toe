acaa3bb8a6fde3eb992c270db31c5a176498239e
Ryle Edwards <ryle.edwards@gmail.com>
2022-04-26 00:12:55 -0500

    Create gameBoard and gameState modules

---
+++ app.js
@@ -0,0 +1,29 @@
+// gameBoard Module
+const gameBoard = (() => {
+  let _board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => {
+    return _board;
+  };
+
+  return {
+    getBoard,
+  };
+})();
+
+console.log(gameBoard.getBoard());
+
+// gameState Module
+const gameState = (() => {
+  let turn = 0;
+  const getTurn = () => {
+    return turn;
+  };
+  const incrementTurn = () => {
+    turn++;
+  };
+  return {
+    getTurn,
+    incrementTurn,
+  };
+})();
